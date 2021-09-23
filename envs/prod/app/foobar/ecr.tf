fc5e85dafd56934702a62fbfdf3ef82383d8ebd0
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-09-23 19:30:39 +0900

    add ecr ポリシーの追加

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -5,3 +5,24 @@ resource "aws_ecr_repository" "nginx" {
     "Name" = "exaple-prod-foobar-nginx"
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "nginx" {
+  policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Hold only 10 images, expire all others",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 10
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+
+  repository = aws_ecr_repository.nginx.name
+}
