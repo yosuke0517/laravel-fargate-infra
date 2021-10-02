b0b2bc89a2cd6bc3c6866135410f86a01f4da2a6
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-02 13:09:27 +0900

    fix: create db

--- envs/prod/network/main/outputs.tf
+++ envs/prod/network/main/outputs.tf
@@ -6,6 +6,10 @@ output "security_group_vpc_id" {
   value = aws_security_group.vpc.id
 }
 
+output "security_group_db_foobar_id" {
+  value = aws_security_group.db_foobar.id
+}
+
 output "subnet_public" {
   # 複数取れる
   value = aws_subnet.public
@@ -17,4 +21,8 @@ output "subnet_private" {
 
 output "vpc_this_id" {
   value = aws_vpc.this.id
+}
+
+output "db_subnet_group_this_id" {
+  value = aws_db_subnet_group.this.id
 }
