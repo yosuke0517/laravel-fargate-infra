9b9d44e1feacce4633f06a36cde48b52c1177486
yosuke0517 <yosuke.takeuchi.dev@gmail.com>
2021-10-03 00:35:58 +0900

    fix: create elastic cache

--- envs/prod/routing/foobar_internal/route53.tf
+++ envs/prod/routing/foobar_internal/route53.tf
@@ -6,6 +6,7 @@ resource "aws_route53_zone" "this" {
   }
 }
 
+# rdsのドメインの名前解決
 resource "aws_route53_record" "db_cname" {
   zone_id = aws_route53_zone.this.zone_id
   name    = "db.${aws_route53_zone.this.name}"
@@ -14,5 +15,17 @@ resource "aws_route53_record" "db_cname" {
 
   records = [
     data.terraform_remote_state.db_foobar.outputs.db_instance_this_address
+  ]
+}
+
+# redisのドメインの名前解決
+resource "aws_route53_record" "cache_cname" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = "cache.${aws_route53_zone.this.name}"
+  type    = "CNAME"
+  ttl     = 300
+
+  records = [
+    data.terraform_remote_state.cache_foobar.outputs.elasticache_replication_group_this_primary_endpoint_address
   ]
 }
